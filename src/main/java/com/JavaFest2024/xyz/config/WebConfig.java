35eeba514e7e9b33fa3c8e68b9e7621550e0867e
Zunaid Ali <zpokean@gmail.com>
2024-08-22 01:23:16 +0600

    Add CORS configuration method

--- src/main/java/com/JavaFest2024/xyz/config/WebConfig.java
+++ src/main/java/com/JavaFest2024/xyz/config/WebConfig.java
@@ -2,6 +2,7 @@
 package com.JavaFest2024.xyz.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user/Profile").setViewName("user/Profile");
         // Add other view controllers as needed
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://sfms-project.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
